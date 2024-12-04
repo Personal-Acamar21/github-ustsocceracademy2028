f1186a3057234bf8319b9e5d0bab9f33719e3906
Aboubacar M Camara <157975662+Personal-Acamar21@users.noreply.github.com>
2024-12-04 13:20:46 -0600

    Update EventCalendar.tsx
    Updated the Event interface in types/cms.ts to include the id property and 'Training' as a valid type

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Calendar, dateFnsLocalizer } from 'react-big-calendar';
-import format from 'date-fns/format';
-import parse from 'date-fns/parse';
-import startOfWeek from 'date-fns/startOfWeek';
-import getDay from 'date-fns/getDay';
+import { format, parse, startOfWeek, getDay } from 'date-fns';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 const locales = {
